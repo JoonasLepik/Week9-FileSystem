0e8482da9fa8f4b3536dab97535fbb77ecf8ee52
Opilane TTHK <opilane@tthk.ee>
2021-10-26 14:05:02 +0300

    First File maker

---
+++ FileSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userDirectory = "wishlist";
+            string newDirectoryFullPath = @$"{rootDirectory}\{userDirectory}";
+            Console.WriteLine(newDirectoryFullPath);
+
+            Directory.CreateDirectory(newDirectoryFullPath);
+        }
+    }
+}
